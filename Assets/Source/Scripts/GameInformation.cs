3c96a2ee3a9faeeef85bb44930e311dfa2aba19c
Ivan <gureff.al@yandex.ru>
2023-08-24 15:00:23 +0300

    Add new script (game information)

---
+++ Assets/Source/Scripts/GameInformation.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+public class GameInformation
+{
+    public void PrintInformation()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Move your player with WASD or arrow keys");
+        Debug.Log("Don't hit the walls!");
+    }
+}
--- Assets/Source/Scripts/Mover.cs
+++ Assets/Source/Scripts/Mover.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     [SerializeField] private float _speed;
-    private void Update()
+    private GameInformation _gameInformation = new GameInformation();
+    private void Start() => _gameInformation.PrintInformation();
+    private void Update() => PlayerMovement();
+    private void PlayerMovement()
     {
         var Horizontal = Input.GetAxis("Horizontal") * Time.deltaTime;
         var Vertical = Input.GetAxis("Vertical") * Time.deltaTime;
