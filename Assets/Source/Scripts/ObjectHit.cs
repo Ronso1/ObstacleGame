d7800b210df2d1ae7fe6e8aa141670c91cbea17c
Ivan <gureff.al@yandex.ru>
2023-08-24 16:59:25 +0300

    Add new scripts

---
+++ Assets/Source/Scripts/ObjectHit.cs
@@ -0,0 +1,5 @@
+using UnityEngine;
+public class ObjectHit : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision) => GetComponent<MeshRenderer>().material.color = Color.red;
+}
---
+++ Assets/Source/Scripts/Scorer.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+public class Scorer : MonoBehaviour
+{
+    private int hits = 0;
+    private void OnCollisionEnter(Collision collision)
+    {
+        hits++;
+        Debug.Log("You've bumped into a thing this many times: " + hits);
+    }
+}
